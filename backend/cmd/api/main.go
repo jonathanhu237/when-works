35624189172d451ae3bce2c43f1f4b71461dbace
jonathanhu237 <jonathanhu237@gmail.com>
2025-10-02 22:03:37 +0800

    feat(config): initialize structured logger with environment config
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jonathanhu237/when-works/backend/internal/config"
+	"github.com/jonathanhu237/when-works/backend/internal/helpers"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	helpers.InitLogger(cfg)
+	slog.Info("logger initialized successfully")
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
