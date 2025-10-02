c3856dc3f8fd9942f1526d63f6fc33464241322a
jonathanhu237 <jonathanhu237@gmail.com>
2025-10-02 22:26:00 +0800

    refactor: remove unnecessary code
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"log/slog"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jonathanhu237/when-works/backend/internal/config"
 	"github.com/jonathanhu237/when-works/backend/internal/helpers"
 )
@@ -18,15 +15,4 @@ func main() {
 
 	helpers.InitLogger(cfg)
 	slog.Info("logger initialized successfully")
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		if _, err := w.Write([]byte("Hello World!")); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	})
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		panic(err)
-	}
 }
